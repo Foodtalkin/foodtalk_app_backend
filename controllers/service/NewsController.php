3f921d6839b22ccf26cd0e00ad2b5d605a01d2be
Shashi Kant Singh <shashi@foodtalkindia.com>
2016-12-21 13:42:41 +0530

    news add and list

---
+++ controllers/service/NewsController.php
@@ -0,0 +1,141 @@
+<?php
+
+class NewsController extends ServiceBaseController
+{
+    public function actionUpsert()
+    {
+        $apiName = 'news/Upsert';
+        $sessionId = null;
+        
+        $_JSON = $this->getJsonInput();
+        
+        try
+        {
+            if(!isset($_JSON) || empty($_JSON))
+                $result = $this->error($apiName, WS_ERR_POST_PARAM_MISSED, 'No input received.');
+            else if(!isset($_JSON['sessionId']) || empty($_JSON['sessionId']))
+                $result = $this->error($apiName, WS_ERR_POST_PARAM_MISSED, 'Please enter session id.');
+            else
+            {
+//             	$user = true;
+                $userId = $this->isAuthentic($_JSON['sessionId']);
+                $user = User::model()->findByPk($userId);
+
+                if (is_null($user))
+                    $result = $this->error($apiName, WS_ERR_WONG_USER, 'Please login before using this service.');
+                else
+                {
+
+                	if(isset($_JSON['id']) && !empty ($_JSON['id'])){
+                		$news = News::model()->findByPk($_JSON['id']);
+                		if(empty($news)){
+                			throw new Exception(print_r('Invalid news id', true), WS_ERR_WONG_VALUE);
+                		}
+                	}
+                	else
+	                	$news = new News('create_api');
+                	
+                	if(isset($_JSON['title']) && !empty($_JSON['title']))
+                		$news->title = filter_var($_JSON['title'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);
+                	 
+                	if(isset($_JSON['coverImage']) && !empty($_JSON['coverImage']))
+                		$news->coverImage = filter_var($_JSON['coverImage'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);
+                	
+                	if(isset($_JSON['source']) && !empty($_JSON['source']))
+                		$news->source = filter_var($_JSON['source'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);
+                	
+                	if(isset($_JSON['sourceUrl']) && !empty($_JSON['sourceUrl']))
+                		$news->sourceUrl = filter_var($_JSON['sourceUrl'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);
+                	
+                	if(isset($_JSON['description']) && !empty($_JSON['description']))
+                		$news->description = filter_var($_JSON['description'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);
+                	
+                	if(isset($_JSON['startDate']) && !empty($_JSON['startDate']))
+                		$news->startDate = filter_var($_JSON['startDate'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);
+                	
+                	if(isset($_JSON['isDisabled']) && !empty($_JSON['isDisabled']))
+                		$news->isDisabled = filter_var($_JSON['isDisabled'], FILTER_SANITIZE_NUMBER_INT);
+                	
+                	if(isset($_JSON['disableReason']) && !empty($_JSON['disableReason']))
+                		$news->disableReason = filter_var($_JSON['disableReason'], FILTER_SANITIZE_STRING | FILTER_SANITIZE_MAGIC_QUOTES);                	
+                	 
+                	$news->save();
+                	
+                	if ($news->hasErrors()){
+                		throw new Exception(print_r($news->getErrors(), true), WS_ERR_UNKNOWN);
+                	}
+                	
+                	$result = array(
+                			'api' => $apiName,
+                			'apiMessage' => 'news updated successfully.',
+                			'status' => 'OK',
+                			'news' => $news->id
+                			
+                	);
+                }
+            }
+        } 
+        catch (Exception $e)
+        {
+            $result = $this->error($apiName, $e->getCode(), Yii::t('app', $e->getMessage()));
+        }
+        $this->sendResponse(json_encode($result, JSON_UNESCAPED_UNICODE));
+    }
+    
+    public function actionList()
+    {
+    	$apiName = 'news/list';
+    	$sessionId = null;
+    
+    	$_JSON = $this->getJsonInput();
+    	
+    	try
+    	{
+    		if(!isset($_JSON) || empty($_JSON))
+    			$result = $this->error($apiName, WS_ERR_POST_PARAM_MISSED, 'No input received.');
+    		else if(!isset($_JSON['sessionId']) || empty($_JSON['sessionId']))
+    			$result = $this->error($apiName, WS_ERR_POST_PARAM_MISSED, 'Please enter session id.');
+    		else
+    		{
+    			//             	$user = true;
+    			$userId = $this->isAuthentic($_JSON['sessionId']);
+    			$user = User::model()->findByPk($userId);
+    	
+    			if (is_null($user))
+    				$result = $this->error($apiName, WS_ERR_WONG_USER, 'Please login before using this service.');
+    			else
+    			{
+    				
+    				$page = 1;
+    				$storeOffer = array();
+    				$options = array();
+    				
+    				if(isset($_JSON['page']) && $_JSON['page'])
+    					$page = filter_var($_JSON['page'], FILTER_SANITIZE_NUMBER_INT);
+    				
+    				$status = 'active';
+    				
+    				if(isset($_JSON['status']) && $_JSON['status'] )
+    					$status = filter_var($_JSON['status'], FILTER_SANITIZE_STRING);
+    				
+    				$news = News::getnews($page, $status, $options);
+    				
+    				$result = array(
+    						'api' => $apiName,
+    						'apiMessage' => 'Records fetched successfully',
+    						'status' => 'OK',
+    						'news' => $news
+    				);
+    				
+    			}
+    		}
+    	}
+    	catch (Exception $e)
+    	{
+    		$result = $this->error($apiName, $e->getCode(), Yii::t('app', $e->getMessage()));
+    	}
+    	$this->sendResponse(json_encode($result, JSON_UNESCAPED_UNICODE));
+    
+    }
+    
+}
---
+++ models/News.php
@@ -0,0 +1,173 @@
+<?php
+
+/**
+ * This is the model class for table "news".
+ *
+ * The followings are the available columns in table 'news':
+ * @property string $id
+ * @property string $title
+ * @property string $coverImage
+ * @property string $source
+ * @property string $sourceUrl
+ * @property string $description
+ * @property string $startDate
+ * @property integer $isDisabled
+ * @property string $disableReason
+ * @property string $createDate
+ * @property string $updateDate
+ * @property string $createId
+ * @property string $updateId
+ */
+class News extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'news';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('title, coverImage, description', 'required'),
+			array('isDisabled', 'numerical', 'integerOnly'=>true),
+			array('title', 'length', 'max'=>200),
+			array('coverImage, sourceUrl', 'length', 'max'=>255),
+			array('source', 'length', 'max'=>50),
+			array('disableReason', 'length', 'max'=>128),
+			array('createId, updateId', 'length', 'max'=>10),
+			array('startDate, updateDate', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, title, coverImage, source, sourceUrl, description, startDate, isDisabled, disableReason, createDate, updateDate, createId, updateId', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'title' => 'Title',
+			'coverImage' => 'Cover Image',
+			'source' => 'Source',
+			'sourceUrl' => 'Source Url',
+			'description' => 'Description',
+			'startDate' => 'Start Date',
+			'isDisabled' => 'Is Disabled',
+			'disableReason' => 'Disable Reason',
+			'createDate' => 'Create Date',
+			'updateDate' => 'Update Date',
+			'createId' => 'Create',
+			'updateId' => 'Update',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('coverImage',$this->coverImage,true);
+		$criteria->compare('source',$this->source,true);
+		$criteria->compare('sourceUrl',$this->sourceUrl,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('startDate',$this->startDate,true);
+		$criteria->compare('isDisabled',$this->isDisabled);
+		$criteria->compare('disableReason',$this->disableReason,true);
+		$criteria->compare('createDate',$this->createDate,true);
+		$criteria->compare('updateDate',$this->updateDate,true);
+		$criteria->compare('createId',$this->createId,true);
+		$criteria->compare('updateId',$this->updateId,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	public static function getQuery($isManager = false)
+	{
+	
+        $sql = "SELECT id";
+        $sql .= ",IFNULL(title, '') as title";
+        $sql .= ",IFNULL(coverImage, '') as coverImage";
+        $sql .= ",IFNULL(source, '') as source";
+        $sql .= ",IFNULL(sourceUrl, '') as sourceUrl";
+        $sql .= ",IFNULL(description, '') as description";
+        $sql .= ",IFNULL(startDate, '') as startDate";
+        $sql .= ",IFNULL(isDisabled, '') as isDisabled";
+        $sql .= " FROM news";
+	
+		return $sql;
+	}
+	
+	public static function getnews($page=1, $status = 'upcomming', $options=array(), $recordCount=9 ){
+	
+		$pagestart = ($page-1) * $recordCount;
+	
+		$sql = self::getQuery();
+	
+		echo $status;
+		
+		if($status != 'all'){
+// 			$sql .= ' WHERE startDate < now() ';
+			$sql .= ' WHERE isDisabled = 0 ';
+		}
+	
+		$sql .= ' ORDER BY createDate ASC';
+	
+		$sql .= ' LIMIT '. $pagestart .', '. $recordCount;
+	
+		echo $sql;
+		
+		$result = Yii::app()->db->createCommand($sql)->queryAll(true);
+		return $result;
+	
+	}
+	
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return News the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
